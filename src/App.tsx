cac41ffbded9bc22a87074a0729830b538481ace
aanvarov <anvarov2295@gmail.com>
2022-09-29 17:51:18 +0500

    cpanel yaml added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
 import PairsGame from "./games/PairsGame/PairsGame";
-import { app, analytics } from "./firebase";
+import { getAnalytics } from "firebase/analytics";
+import { app } from "./firebase";
 
 function App() {
-  console.log(analytics.app.options.appId);
-
+  getAnalytics(app);
   return (
     <div className="App">
       <PairsGame />
